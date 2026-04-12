01b4e419c187526a90c29ccc84173244ffeb2981
Raouf2 <raouftouati122@gmail.com>
2026-04-12 22:32:32 +0100

    practice

--- exo3.py
+++ exo3.py
@@ -1,4 +1,3 @@
-
 def main():
     number = int(input("Enter your number "))
     if is_even(number):
@@ -7,11 +6,30 @@ def main():
         print("odd")
 
 
+    #check if number from 0 to 100 are permier or not
+
+    for i in range(101):
+        print(f"{i} est : {is_premier(i)}")
+
 def is_even(number):
     if number % 2 == 0:
         return True
 
     return False
+
+
+def is_pair(number):
+    return (number % 2 == 0)
+
+def is_premier(number):
+    count = number - 1
+    while count >= 2:
+        if number % count == 0:
+            return False
+
+        count -= 1
+
+    return True
 
 
 
