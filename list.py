193610f76a62ac310c980f2fb77bd689101d4c24
Raouf2 <raouftouati122@gmail.com>
2026-04-16 21:56:24 +0100

    loops in python

---
+++ list.py
@@ -0,0 +1,25 @@
+colors = ["red" , "blue" , "yellow"]
+
+names = ["raouf" , "cherifa" , "hello" , "world" , "python"]
+
+
+students = {
+    "raouf" : "red",
+    "cherifa" : "blue",
+    "hello" : "yellow"
+        }
+
+for student in students:
+    print(student, students[student], sep=", ")
+
+
+
+student = [
+    {"name":"raouf" , "house":"bejaia" , "age":24},
+    {"name":"cherifa", "house":"timerit", "age":22},
+    {"name":"Hello", "house":"world" , "age":999}
+]
+
+for s in student:
+    print(s["name"], s["house"] , s["age"] , sep=", ")
+
--- loop2.py
+++ loop2.py
@@ -1,1 +1,20 @@
-print("raouf\n" * 2 , end="")
+def main():
+    n = int(input("Enter n : "))
+    word = input("Enter the word to print n times")
+    print_word(n , word)
+
+
+def print_word(n , word):
+    if n <= 0 or word == "":
+        print("N is negative or you word is empty")
+        return
+    
+    for _ in range(n):
+        print(word)
+
+
+
+
+
+
+main()
