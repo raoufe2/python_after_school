54f7d5372623b95ec49d85a8f5cc750aaf997ec1
Raouf2 <raouftouati122@gmail.com>
2026-04-28 19:43:41 +0100

    use packages and modules in python

--- sys.py
+++ sys.py
@@ -1,17 +1,31 @@
 import sys
-
+import cowsay
 
 def main():
-    if len(sys.argv) != 2:
-        print("USE: python3 sys.py YOU_NAME")
-        return 
+    if len(sys.argv) < 2:
+        ## affichie un message 
+        ## print("Too few arguments")
+        sys.exit("Too few Arguments")
+    
+    for arg in sys.argv:
+        print(f"{arg}")
 
-    print(sys.argv[1])
+    #use slices
+    print("Print using slices:")
+    for arg in sys.argv[1:]:
+        print(f"arg : {arg}")
 
+    #sys.version
+    print(sys.version)
+    print(sys.platform)
+    
+    #use the package cowsay by the package cowsay installed by pip
 
+    cowsay.cow("MOOOOOOOO")
+    
+    my_fish = "r'''"
 
-
-
+    cowsay.trex("Hi iam a fish")
 
 
 main()
